64659e235d63a733b0172e210037e2e444635593
Ian <ileaman1@gmail.com>
2014-07-17 21:59:41 -0600

    added basic world generation and parsing. Next up is improved parsing and interactions

--- CreateYourOwnAdventure.py
+++ CreateYourOwnAdventure.py
@@ -14,6 +14,8 @@ def main():
 class Space:
 	def __init__(self, spaceType):
 		self.type = spaceType
+	def __unicode__(self):
+		return "Space object of type "+ self.type  
 
 class Grid:
 	def __init__(self,x,y,z):
@@ -24,9 +26,9 @@ class Grid:
 		}
 		
 		self.worldBounds = {
-			"x-up":None,
+			"x-up":9999,  #Make none in future
 			"x-down":0,
-			"y-up":None,
+			"y-up":9999,
 			"y-down":0,
 			"z-up":0,
 			"z-down":0,
@@ -42,6 +44,111 @@ class Grid:
 	def addSpace(self, x,y,z,space):
 		self.grid[x][y][z] = space
 
+	def incrementXPosUp(self):
+
+			self.currentLocation["x"] += 1
+			if not self.checkBounds():
+				self.currentLocation["x"] -= 1
+				print("Can't move in that direction. Sorry.")
+
+			
+
+			spaceObject = Space("mountan")
+			self.insertAtPostition(x = self.currentLocation["x"],
+				y = self.currentLocation["y"], z = self.currentLocation["z"], 
+				spaceObject = spaceObject)
+
+
+
+	def incrementXPosDown(self):
+		self.currentLocation["x"] -= 1
+		if not self.checkBounds():
+			self.currentLocation["x"] += 1
+			print("Can't move in that direction. Sorry.")
+
+		spaceObject = Space("mountan")
+		self.insertAtPostition(x = self.currentLocation["x"],
+			y = self.currentLocation["y"], z = self.currentLocation["z"], 
+			spaceObject = spaceObject)
+
+
+	def incrementYPosUp(self):
+		self.currentLocation["y"] += 1
+		if not self.checkBounds():
+			self.currentLocation["y"] -= 1
+			print("Can't move in that direction. Sorry.")
+
+		spaceObject = Space("mountan")
+		self.insertAtPostition(x = self.currentLocation["x"],
+			y = self.currentLocation["y"], z = self.currentLocation["z"], 
+			spaceObject = spaceObject)
+
+	def incrementYPosDown(self):
+		self.currentLocation["y"] -= 1
+		if not self.checkBounds():
+			self.currentLocation["y"] += 1
+			print("Can't move in that direction. Sorry.")
+
+		spaceObject = Space("mountan")
+		self.insertAtPostition(x = self.currentLocation["x"],
+			y = self.currentLocation["y"], z = self.currentLocation["z"], 
+			spaceObject = spaceObject)
+
+	def incrementZPosUp(self):
+		self.currentLocation["z"] += 1
+		if not self.checkBounds():
+			self.currentLocation["z"] -= 1
+			print("Can't move in that direction. Sorry.")
+
+		spaceObject = Space("mountan")
+		self.insertAtPostition(x = self.currentLocation["x"],
+			y = self.currentLocation["y"], z = self.currentLocation["z"], 
+			spaceObject = spaceObject)
+
+	def incrementZPosDown(self):
+		self.currentLocation["z"] -= 1
+		if not self.checkBounds():
+			self.currentLocation["z"] += 1
+			print("Can't move in that direction. Sorry.")
+
+		spaceObject = Space("mountan")
+		self.insertAtPostition(x = self.currentLocation["x"],
+			y = self.currentLocation["y"], z = self.currentLocation["z"], 
+			spaceObject = spaceObject)
+
+	def checkBounds(self):
+
+		
+		if ((self.currentLocation["x"] <= self.worldBounds["x-up"]) and (
+			self.currentLocation["x"] >= self.worldBounds["x-down"]) and 
+			((self.currentLocation["y"] <= self.worldBounds["y-up"]) and (
+			self.currentLocation["y"] >= self.worldBounds["y-down"])) and
+			((self.currentLocation["z"] <= self.worldBounds["z-up"]) and (
+			self.currentLocation["z"] >= self.worldBounds["z-down"]))):
+				return True
+		else:
+			return False
+
+	def insertAtPostition(self, x, y,z, spaceObject):
+		if x not in self.grid:
+			self.grid[x] = {}
+		if y not in self.grid[x]:
+			self.grid[x][y] = {}
+		if z not in self.grid[x][y]:
+			self.grid[x][y][z] = spaceObject
+			print("New Object Created")
+
+	def getCurrentSpaceObject(self):
+		x = self.currentLocation["x"]
+		y = self.currentLocation["y"]
+		z = self.currentLocation["z"]
+		return self.grid[x][y][z]
+
+	def describeLocation(self):
+		print("At location of type "+ self.getCurrentSpaceObject().type +
+				" at coordinants: " + str(self.currentLocation) )
+
+
 class Game():
 	def __init__(self):
 
@@ -50,20 +157,70 @@ class Game():
 		self.grid.addSpace(0,0,0, startSpace)
 
 		self.actionMap = {
-			"up":incrementPosUp,
-			"down",incrementPosDown, 
-			"describe":
+			"move":{
+				"x-up":self.grid.incrementXPosUp,
+				"x-down":self.grid.incrementXPosDown,
+				"y-up":self.grid.incrementYPosUp,
+				"y-down":self.grid.incrementYPosDown,
+				"z-up":self.grid.incrementZPosUp,
+				"z-down":self.grid.incrementZPosDown,
+			},
+			"describe":{
+				"location":self.grid.describeLocation,
+			}
 		}
 
 	def start(self):
 		command = "You find yourself in an empty room, what would you like to do?"
-		while True:
-			
-			command = self.parse(input(command + " "))
+		loop = True
+		while loop:
+
+			loop = self.parse(input(command + " "))
 
 
-	def parse(self, command):
-		pass
+	def parse(self, commandString):
+		commandList = commandString.split(" ")
+
+		i = 0
+		while i < len(commandList):
+			word = commandList[i]
+
+			if word.lower() == "go" or word.lower() == "move" or word.lower() == "travel":
+				i += 1
+				word = commandList[i].lower()
+				if word == "south":
+					self.actionMap["move"]["x-up"]()
+				elif word == "north":
+					self.actionMap["move"]["x-down"]()
+				elif word == "east":
+					self.actionMap["move"]["y-up"]()
+				elif word == "west":
+					self.actionMap["move"]["y-down"]()
+				elif word == "up":
+					self.actionMap["move"]["z-up"]()
+				elif word == "down":
+					self.actionMap["move"]["z-down"]()
+				else:
+					print("Go " + word +"? Nah.")
+
+			if word.lower() == "describe":
+				i += 1
+				# print(i, commandList )
+				word = commandList[i].lower()
+
+				if word == "location":
+					self.actionMap["describe"]["location"]()
+
+
+			if word.lower() == "exit()" and len(commandList) == 1:
+				if input("Are you sure? There is currently no save feature. ")[:1].lower() == "y":
+					return False
+
+			i += 1
+		return True
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ TextAdventure
 =============
 
 Create your own text based adventure!
+
+This game was made for the Tufts weekly hacking chalange and is based on Zork.
